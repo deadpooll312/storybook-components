962f450f631980c480148d720d5102850972290f
kapar.zhekshen@carvago.com <admin@admins-MacBook-Pro.local>
2022-02-09 12:58:03 +0600

    first comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "./stories/Button";
 
 function App() {
   return (
@@ -10,14 +11,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button
+          label="Button"
+          onClick={() => {}}
+          primary
+        />
       </header>
     </div>
   );
